9323d5085aef4850c297f907bd9c332162765386
JorgeRomojaro11 <jromodel@myuax.com>
2024-02-21 11:18:27 +0100

    .

---
+++ Cronómetro_Cósmico.java
@@ -0,0 +1,8 @@
+public class Planeta {
+    private final double longitudDelDiaEnSegundos;
+    private final double longitudDelAñoEnDias;
+
+    public Planeta(double longitudDelDiaEnSegundos, double longitudDelAñoEnDias) {
+        this.longitudDelDiaEnSegundos = longitudDelDiaEnSegundos;
+        this.longitudDelAñoEnDias = longitudDelAñoEnDias;
+    }
