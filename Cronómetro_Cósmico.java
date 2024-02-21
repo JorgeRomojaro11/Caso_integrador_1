1f7be51b050a3c0c40587aa09c707e7012502e59
JorgeRomojaro11 <jromodel@myuax.com>
2024-02-21 11:21:27 +0100

    .

--- Cronómetro_Cósmico.java
+++ Cronómetro_Cósmico.java
@@ -24,3 +24,15 @@ public class CronómetroCósmico {
         double diasPlaneta = diasTierra * (LONGITUD_DIA_TIERRA_EN_SEGUNDOS / planeta.getLongitudDelDiaEnSegundos());
         return diasPlaneta * planeta.getLongitudDelDiaEnSegundos();
     }
+    public void mostrarTiempoEnVariosFormatos(double tiempoEnSegundos) {
+        double minutos = tiempoEnSegundos / 60;
+        double horas = minutos / 60;
+        double dias = horas / 24;
+        double años = dias / LONGITUD_AÑO_TIERRA_EN_DIAS;
+
+        System.out.println("Tiempo en segundos: " + tiempoEnSegundos);
+        System.out.println("Tiempo en minutos: " + minutos);
+        System.out.println("Tiempo en horas: " + horas);
+        System.out.println("Tiempo en días: " + dias);
+        System.out.println("Tiempo en años: " + años);
+    }
